c2ddd9ef237bd13bee46cc0cc17397893f7c2203
fahmi-1997 <fahmialamsyah97@gmail.com>
2021-10-18 10:51:18 +0700

    add & delete todo done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,87 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { findOne, removeTodo } from "../redux/actions/todoActions";
+import Modal from "react-modal";
+import moment from "moment";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: "400px",
+  },
+};
+
+Modal.setAppElement("#root");
 
 const Todo = ({ data }) => {
   const dispatch = useDispatch();
+
   const handleRemove = (id) => {
     dispatch(removeTodo(id));
+    closeModal();
   };
 
   const handleUpdate = (id) => {
     dispatch(findOne(id));
   };
 
+  // modal functionality
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="my-2">
+    <div className="mb-2">
       <div className="card p-2" style={{ cursor: "pointer" }} onDoubleClick={() => handleUpdate(data.id)}>
         <div className="d-flex align-items-center justify-content-between">
-          <p className="m-0" style={{ fontSize: "20px" }}>
+          <p className="m-0 text-justify" style={{ fontSize: "20px" }}>
             {data.task}
           </p>
-          <button onClick={() => handleRemove(data.id)} className="btn btn-danger">
-            DELETE
+
+          <button className="btn btn-success" onClick={openModal}>
+            Details
           </button>
         </div>
       </div>
+
+      {/* modal */}
+
+      <Modal isOpen={modalIsOpen} onAfterOpen={afterOpenModal} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>{data.task}</h2>
+        <div>
+          Date: <p>{moment(data.date).format("MMMM Do YYYY")}</p>
+          Status<p>{data.status}</p>
+        </div>
+
+        <div className="w-100">
+          <div className="d-flex btn-group mb-2">
+            <button onClick={() => handleRemove(data.id)} className="btn btn-block btn-danger">
+              DELETE
+            </button>
+            <button className="btn btn-block btn-info">UPDATE</button>
+          </div>
+        </div>
+        <button className="btn btn-primary" onClick={closeModal}>
+          CLOSE
+        </button>
+      </Modal>
     </div>
   );
 };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,16 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import Todo from "../components/Todo";
 import { addTodo, emptyTodo } from "../redux/actions/todoActions";
 import "./home.css";
+
 const Home = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.allTodos.todos);
   const todo = useSelector((state) => state.allTodos.todo);
   const [input, setInput] = useState("");
+  const [taskDate, setTaskDate] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (input === "") {
+    if (input === "" || taskDate === "") {
       return;
     }
 
@@ -28,6 +30,7 @@ const Home = () => {
       dispatch(
         addTodo({
           id: new Date(),
+          date: taskDate,
           task: input,
           status: 0,
         })
@@ -42,33 +45,55 @@ const Home = () => {
     setInput(todo.task);
   }, [todo]);
 
-  // status 0; belum selesai
+  // status 0; belum selesai; sort ASC
+  const pending = todos
+    .filter((i) => i.status === 0)
+    .sort(function (a, b) {
+      return new Date(a.date) - new Date(b.date);
+    });
 
-  const pending = todos.filter((i) => i.status === 0);
-  const done = todos.filter((i) => i.status !== 0);
+  // status 1; done; sort DSC
+  const done = todos
+    .filter((i) => i.status !== 0)
+    .sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date);
+    });
 
   return (
     <div className="container">
-      <div className="home">
-        <h1>todos</h1>
-        <form onSubmit={handleSubmit} className="d-flex flex-column mb-3">
-          <input className="my-1" id="new-todo" type="text" value={input} onChange={(e) => setInput(e.target.value)} autoComplete="off" />
-          <button className="my-1 btn btn-primary" type="submit">
-            ADD
-          </button>
-        </form>
+      <div className="row">
+        <div className="col-lg-4 text-center pt-2">
+          <h3>Add New Todo</h3>
+          <form onSubmit={handleSubmit}>
+            {/* <div className="form-group">
+              <input className="mb-2 form-control" id="new-todo" type="text" value={input} onChange={(e) => setInput(e.target.value)} autoComplete="off" />
+            </div> */}
 
-        <div className="row">
-          <div className="col-lg-6">
-            {pending.map((e, i) => (
-              <Todo key={i} data={e} />
-            ))}
-          </div>
-          <div className="col-lg-6">
-            {done.map((e, i) => (
-              <Todo key={i} data={e} />
-            ))}
-          </div>
+            <div className="form-floating">
+              <textarea className="form-control" id="floatingTextarea2" value={input} onChange={(e) => setInput(e.target.value)} style={{ height: "100px" }}></textarea>
+              <label htmlFor="floatingTextarea2">Enter task here...</label>
+            </div>
+
+            <div className="form-group">
+              <input id="myDate" type="date" onChange={(e) => setTaskDate(e.target.value)} className="my-1 form-control" />
+            </div>
+
+            <button className="my-2 btn btn-primary" type="submit">
+              SUBMIT
+            </button>
+          </form>
+        </div>
+        <div className="col-lg-4 text-center pt-2">
+          <h3>On Going</h3>
+          {pending.map((e, i) => (
+            <Todo key={i} data={e} />
+          ))}
+        </div>
+        <div className="col-lg-4 text-center pt-2">
+          <h3>Done</h3>
+          {done.map((e, i) => (
+            <Todo key={i} data={e} />
+          ))}
         </div>
       </div>
     </div>
